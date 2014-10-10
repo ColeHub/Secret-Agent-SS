613767d060ee88981311cc798042dbe8f0a15ac5
Cole Hubscher <cole@colembp.local>
2014-10-10 12:06:12 -0700

    Initial commit

---
+++ desktop/src/com/afternooncoffeesoftware/sass/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.afternooncoffeesoftware.sass.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.afternooncoffeesoftware.sass.SASS;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SASS(), config);
+	}
+}
---
+++ html/src/com/afternooncoffeesoftware/sass/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.afternooncoffeesoftware.sass.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.afternooncoffeesoftware.sass.SASS;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new SASS();
+        }
+}
