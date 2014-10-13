2d74e984bf155f0fad7e6a01b38fac86e26a5542
Windovvsill <svezer@live.ca>
2014-10-12 17:54:52 -0700

    Merge branch 'master' of C:\Users\Steve\dev\Secret-Agent-SS_ with conflicts.

--- core/src/com/afternooncoffeesoftware/sass/Art.java
+++ core/src/com/afternooncoffeesoftware/sass/Art.java
@@ -1,10 +1,12 @@
 package com.afternooncoffeesoftware.sass;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 
 /**
  * Created by cole on 2014-10-10.
@@ -12,29 +14,27 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class Art {
     public static SpriteBatch batch;
     public static OrthographicCamera camera;
-    public static Texture playerImg;
-    public static TextureRegion playerReg;
-    public static Texture titleImg;
-    public static Texture lampImg;
-    public static Sprite titleSprite;
-    public static Sprite lampSprite;
+    public static Texture img;
+    public static Sprite sprite;
+    public static Rectangle player;
 
 
     public static void load() {
+
         batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
 
-        playerImg = new Texture("nekkid.png");
+        player = new Rectangle();
+        player.x = (800 / 3) * 2;
+        player.y = (480 / 3);
+        player.setHeight(64);
+        player.setWidth(64);
 
-        titleImg = new Texture("title.png");
-        titleSprite = new Sprite(titleImg);
-        titleSprite.scale(3);
-        titleSprite.setOrigin(0, 0);
-        titleSprite.setPosition(0, 0);
-
-        lampImg = new Texture("lamp.png");
-        lampSprite = new Sprite(lampImg);
-        lampSprite.scale(4);
-        lampSprite.setPosition(200, 200);
+        img = new Texture("nekkid.png");
+        sprite = new Sprite(img);
+        sprite.scale(4);
+        sprite.setPosition(player.x, player.y);
 
     }
 }
